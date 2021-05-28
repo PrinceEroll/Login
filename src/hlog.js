e58af3f3536af61b6ae287c37340a690faa4031b
DrPlague1210 <drplague1210@gmail.com>
2021-05-28 17:09:35 +0800

    First commit

---
+++ src/hlog.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {ReactComponent as Logo} from './login.svg'
+class Login extends React.Component{
+state={
+email:'',
+pwd:''
+}
+handleChange = (e) =>{
+const {name,value} = e.target
+this.setState({[name]:value})
+}
+handleSubmit = (e) =>{
+}
+render(){
+return(
+<div>
+<div>
+<Logo/>
+</div>
+<form onSubmit = {this.handleSubmit}>
+<input type='email' name='email' placeholder='email...' required onChange={this.handleChange}/>
+<input type='password' name='pwd' placeholder='password...' required onChange={this.handleChange}/>
+<button onSubmit={this.handleSubmit}>Log In</button>
+</form>
+</div>
+)
+}
+}
+export default Login;
